bf69faf37a88d67ccb019adccace6757bf864002
Mehul789 <mehul.excelsior@gmail.com>
2019-07-25 13:14:14 +0530

    Mehul

---
+++ app/src/main/java/com/example/AutoBell/Activity/DashbordActivity.java
@@ -0,0 +1,17 @@
+package com.example.AutoBell.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.autobell.R;
+
+public class DashbordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashbord);
+    }
+}
