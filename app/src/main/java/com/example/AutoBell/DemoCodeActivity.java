f8dc28837392bc0c4ac713da39768637f215ff18
ExT-EMP-NPC-008 <yogesh.excelsior@gmail.com>
2019-07-25 12:59:12 +0530

    Shruti

--- app/src/main/java/com/example/AutoBell/DemoCodeActivity.java
+++ app/src/main/java/com/example/AutoBell/DemoCodeActivity.java
@@ -52,14 +52,6 @@ public  class DemoCodeActivity extends AppCompatActivity implements ImageChooser
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Log.e("Pashva","Pashva");
-        Log.e("Shruti","Shruti");
-        Log.e("Mehul","Mehul");
-        Log.e("Yogesh","Yogesh");
-        Log.e("Rajal","Rajal");
-        Log.e("Parshva########","Parshva########");
-        Log.e("Parshva########","******");
         Button buttonTakePicture = (Button) findViewById(R.id.buttonTakePicture);
         buttonTakePicture.setOnClickListener(new View.OnClickListener() {
 
