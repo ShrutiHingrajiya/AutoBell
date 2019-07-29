4c0e65a0b0f51eb977da8bb0bfe48e52e00e3d99
Mehul789 <mehul.excelsior@gmail.com>
2019-07-29 14:30:09 +0530

    Merge branch 'master' of D:\Mehul_Work\AutoBell\25-07-2019\AutoBellNew with conflicts.

--- app/src/main/java/com/example/AutoBell/Activity/DashbordActivity.java
+++ app/src/main/java/com/example/AutoBell/Activity/DashbordActivity.java
@@ -1,17 +1,38 @@
 package com.example.AutoBell.Activity;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.example.AutoBell.Utils.MenuDialog;
 import com.example.autobell.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class DashbordActivity extends AppCompatActivity {
 
+
+    @BindView(R.id.img_dashboard_menu)
+    ImageView imgDashboardMenu;
+    @BindView(R.id.ll_main)
+    LinearLayout llMain;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashbord);
+        ButterKnife.bind(this);
+
+
+    }
+
+    @OnClick(R.id.img_dashboard_menu)
+    public void onViewClicked()
+    {
+        MenuDialog.OpenMenuDialog(DashbordActivity.this);
     }
 }
